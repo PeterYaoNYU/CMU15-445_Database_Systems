9eff7d9aaefeb0d985d889949f7e58194eca0cdc
Peter Yao <yy4108@nyu.edu>
2022-08-26 19:58:41 +0800

    lru_replacer class implemeted, dont know if it work or not

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,48 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+
+    
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool {
+    if (lru_replacer.empty()){
+        return false;
+    }
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+    replacer_mutex.lock();
+    
+    int frame_id_min = lru_replacer.begin()->first;
+    frame_id_t timestamp_min = lru_replacer.begin()->second;
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+    for (auto it = lru_replacer.begin(); it!=lru_replacer.end(); ++it){
+        if (it->second < timestamp_min){
+            timestamp_min = it->second;
+            frame_id_min = it->first;
+        }
+    }
+    * frame_id = frame_id_min;
+    lru_replacer.erase(frame_id_min);
 
-auto LRUReplacer::Size() -> size_t { return 0; }
+    replacer_mutex.unlock();
+    return true;
+}
+
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    replacer_mutex.lock();
+    lru_replacer.erase(frame_id);
+    replacer_mutex.unlock();
+}
+
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    replacer_mutex.lock();
+    lru_replacer.insert({frame_id, timestamp++});
+    replacer_mutex.unlock();
+}
+
+auto LRUReplacer::Size() -> size_t { return lru_replacer.size(); }
 
 }  // namespace bustub
